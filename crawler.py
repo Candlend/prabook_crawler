24eec71bcd0ca3da7e5ea087cd6b22cdd31a0204
Candlend <huangchy@shanghaitech.edu.cn>
2018-01-17 17:38:20 +0800

    finish

--- crawler.py
+++ crawler.py
@@ -7,42 +7,49 @@ import json
 import regex as re
 import Levenshtein
 
-def crawl(name):
-    switch = 0
+def crawl(name, number, strict, limit):
+    print ("[%d] Crawling..." % number)
     celebrity = {'Education':'','Personality':'','Background':'','Birthday':'','Birthplace':'','Foreign':'','Career':'','Connections-Married':'', \
-    'father':'','mother':'','spouse':'','how many children':'','children':'','name':''}
+    'father':'','mother':'','spouse':'','how many children':'','children':'','name':'','reliability':0}
     url= "https://prabook.com/web/search.json"
     p={'general':name,'_dc':int(time.time()),'start':0,'rows':5}
     r1=requests.get(url,params=p)
-    reliability = 0
     for eachresult in r1.json()['result']:
-        celebrity['name']= eachresult["fullName"].replace("<mark>","").replace("</mark>","")
-        eachreliability = Levenshtein.ratio(celebrity['name'].lower().replace('.',''),name.lower().replace('.',''))
-        if eachreliability > reliability:
-            reliability = eachreliability
+        eachname= eachresult["fullName"].replace("<mark>","").replace("</mark>","")
+        eachreliability = Levenshtein.ratio(eachname.lower().replace('.',''),name.lower().replace('.',''))
+        if eachreliability > celebrity["reliability"]:
+            celebrity["reliability"] = eachreliability
             j = eachresult
-        print (celebrity['name'])
-    print ('reliability: %.2f%%' % (reliability*100))
+            celebrity['name'] = eachname
+    print ('[%d] reliability: %.2f%%' % (number,celebrity["reliability"]*100))
+    if  celebrity["reliability"] < limit:
+        print ("[%d] The reliability is too low!" % number)
+        if strict == 1:
+            return celebrity
     celebrity['Background']=j["staticBackground"]
     celebrity['Birthday']="%d/%d/%d" % (j["birthYear"],j["birthMonth"],j["birthDay"])
     celebrity['Birthplace']=j["birthPlace"]
-    if j["nationalities"][0] == "American":
-        celebrity['Foreign'] = 'N'
-    else:
-        celebrity['Foreign'] = 'Y'
+    try:
+        if j["nationalities"][0] == "American":
+            celebrity['Foreign'] = 'N'
+        else:
+            celebrity['Foreign'] = 'Y'
+    except IndexError:
+        pass
     path="https://prabook.com/web" + j["seoUrl"]
     r2=requests.get(path)
     html = r2.text.encode("utf-8")
     tree = etree.HTML(html)
     links = tree.xpath('//article[@class="article__item"]')
     if len(links) == 0:
-        print ("no article")
+        # print ("no article")
         return celebrity
     try:
         Interests=tree.xpath('//p[@class="interest-list__element"]/text()')[0]
         celebrity["Personality"]=Interests.replace("\r","").replace("\n","").replace("\t","").replace("â","—")
     except:
-        print ("no interest")
+        # print ("no interest")
+        pass
     for eachlink in links:
         title1 = eachlink.xpath('h3[@class="article__title"]/text()')[0].replace("\r","").replace("\n","").replace("\t","")
         if title1 == "Education" or title1 == "Career" or title1 == "Background":
@@ -55,10 +62,11 @@ def crawl(name):
                 married = re.findall(r'(?<=Married[^\d:;]+?, )[^.]*(?=.)',text)
                 if len(married) == 1:
                     celebrity["Connections-Married"] = married[0]
-                else:
-                    print ("something wrong %d" % len(married))
+                # else:
+                    # print ("something wrong %d" % len(married))
             except IndexError:
-                print ("He has no wife.")
+                # print ("He has no wife.")
+                pass
             links2 = eachlink.xpath('dl[@class="def-list"]')
             for eachlink2 in links2:
                 try:
@@ -66,15 +74,9 @@ def crawl(name):
                 except IndexError:
                     # print ("no title")
                     pass
-                #     text = eachlink2.xpath('dd[@class="def-list__text"]/text()')[0].replace("\r","").replace("\n","").replace("\t","").replace("â","—").strip()
-                #     if switch == 1:
-                #         celebrity["children"] = celebrity["children"] + "; " + text
-                #         celebrity["how many children"] += 1
-                #     else:
-                #         celebrity["spouse"] = celebrity["spouse"] + "; " + text
                 if title2 == "father:" or title2 == "mother:" or title2 == "spouse:" or title2 == "children:" or title2 == "spouses:":
                     text = eachlink2.xpath('dd[@class="def-list__text"]/text()')[0].replace("\r","").replace("\n","").replace("\t","").replace("â","—").strip()
-                    if title2 == "spouses":
+                    if title2 == "spouses:":
                         if celebrity["spouse"] == "":
                             celebrity["spouse"] = text
                         else:
@@ -94,10 +96,13 @@ def crawl(name):
         else:
             continue
 
+    print ("[%d] Get!" % number)
     return (celebrity)
 
 
 if __name__ == '__main__':
     name = input('name: ')
-    celebrity = crawl(name)
+    strict = input('strict: ')
+    limit = input('limit: ')
+    celebrity = crawl(name, 0, strict, limit)
     print (json.dumps(celebrity,sort_keys=True, indent=4, separators=(',', ': ')))
--- excel.py
+++ excel.py
@@ -2,21 +2,13 @@ import xlrd
 from xlutils.copy import copy
 import sys
 import crawler
+import json
+from multiprocessing.dummy import Pool
 
-rdata = xlrd.open_workbook(sys.argv[1])
-wdata = copy(rdata)
-rtable = rdata.sheets()[0]
-wtable = wdata.get_sheet(0)
-nrows = rtable.nrows
-start = int(input("start: "))-1
-end = int(input("end: "))
-path = input('save position: ')
-for i in range(start,end):
+def process(i):
     name = rtable.row_values(i)[2]
-    print ("Crawling... "+ name)
-    celebrity = crawler.crawl(name)
-    print (celebrity)
-    print ("Get!")
+    celebrity = crawler.crawl(name, i, strict, limit)
+    # print (json.dumps(celebrity,sort_keys=True, indent=4, separators=(',', ': ')))
     wtable.write(i,6,celebrity['Background'])
     wtable.write(i,7,celebrity['Birthday'])
     wtable.write(i,8,celebrity['Birthplace'])
@@ -24,10 +16,33 @@ for i in range(start,end):
     wtable.write(i,10,celebrity['Education'])
     wtable.write(i,11,celebrity['Career'])
     wtable.write(i,12,celebrity['Personality'])
-    wtable.write(i,13,celebrity['Connections-Married'])
+    wtable.write(i,13,celebrity['Connections-Married']))
     wtable.write(i,14,celebrity['father'])
     wtable.write(i,15,celebrity['mother'])
     wtable.write(i,16,celebrity['spouse'])
     wtable.write(i,17,celebrity['how many children'])
     wtable.write(i,18,celebrity['children'])
+    wtable.write(i,35,"%.2f%%" % (celebrity["reliability"]*100))
+    wtable.write(i,36,name)
+    wtable.write(i,37,celebrity['name'])
+
+rdata = xlrd.open_workbook(sys.argv[1])
+wdata = copy(rdata)
+rtable = rdata.sheets()[0]
+wtable = wdata.get_sheet(0)
+nrows = rtable.nrows
+start = int(sys.argv[3])
+end = int(sys.argv[4])+1
+if sys.argv[5]:
+    print ("use strict")
+    strict = 1
+else:
+    strict = 0
+path = sys.argv[2]
+limit = float(sys.argv[6])
+print ("range: %d~%d" % (int(sys.argv[3]),int(sys.argv[4])))
+print ('save position: ' + path)
+print ("the lower limit of reliability: ",limit)
+pool = Pool(processes=4)
+pool.map(process,range(start,end))
 wdata.save(path)
